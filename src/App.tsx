2af0959b90f6b9b90de1774f87f68a805e79e24a
Victor Batalha <madivcb@gmail.com>
2023-09-03 14:43:16 -0300

    clean unused code

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>VoltVision</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
